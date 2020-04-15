8f8990f90000f7c7aafc3f36d49b0323ea84a3a8
Dawid Kamola <dawid@kamola.pl>
2020-04-15 14:34:59 +0200

    Configured postgres db.

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'app',
+        'USER': 'postgres',
+        'PASSWORD': 'd443c210a5874d5b8fe8375c1f1a5298'
     }
 }
 
